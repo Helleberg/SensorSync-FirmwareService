b4e67d9e3fe2d46c49f1df980c6e41856ae151ab
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-04-21 00:49:45 +0200

    first commit

---
+++ src/main/java/dk/sdu/firmwareservice/FirmwareServiceApplication.java
@@ -0,0 +1,13 @@
+package dk.sdu.firmwareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmwareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmwareServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/sdu/firmwareservice/FirmwareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.firmwareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmwareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
