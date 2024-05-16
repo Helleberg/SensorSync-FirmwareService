882766f121e3b008782a85e818a0c97308f19e26
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-05-16 22:02:52 +0200

    Bin generation test

--- src/main/java/dk/sdu/firmwareservice/service/FirmwareService.java
+++ src/main/java/dk/sdu/firmwareservice/service/FirmwareService.java
@@ -39,7 +39,7 @@ public class FirmwareService {
         // Check if latest toit firmware version is newer than the device firmware.
         if (formatFirmwareVersion(latestToitVersion) > formatFirmwareVersion(device.getToit_firmware_version())) {
             // Generate the firmware update file
-            Boolean isFirmwareGenerated = generateFirmware(latestToitVersion);
+            Boolean isFirmwareGenerated = generateFirmware(latestToitVersion, uuid);
             if (isFirmwareGenerated) {
                 // When the firmware has been generated send a message through the MessageService
                 // to allow the device to begin updating.
@@ -67,15 +67,16 @@ public class FirmwareService {
         return gitHubService.getLatestToitRelease();
     }
 
-    public Boolean generateFirmware(String firmwareVersion) {
+    public Boolean generateFirmware(String firmwareVersion, UUID uuid) {
         // TODO: Include ATHENA snapshot somewhere in this logic
         // Right know the latest ATHEA version just gets bundled with the new firmware.
         try {
             // Concatenating the deviceUUID onto the filename to keep track of which device should download it.
             String envelopeUrl = "https://github.com/toitlang/toit/releases/download/" + firmwareVersion + "/firmware-esp32.gz";
-            downloadFile(envelopeUrl, firmwareVersion);
-            gunzipFile(firmwareVersion);
-            generateFirmwareBin(firmwareVersion);
+            makeFirmwareFolder(uuid);
+            downloadFile(envelopeUrl, uuid);
+            gunzipFile(uuid);
+            generateFirmwareBin(uuid);
             return true;
         } catch (IOException e) {
             log.warn(e.getMessage());
@@ -83,12 +84,38 @@ public class FirmwareService {
         }
     }
 
-    private static void downloadFile(String urlStr, String firmwareVersion) throws IOException {
+    private static void makeFirmwareFolder(UUID uuid) {
+        // Create file for the firmware
+        ProcessBuilder processBuilderNewDir = new ProcessBuilder();
+        processBuilderNewDir.command("mkdir "+ uuid.toString());
+        processBuilderNewDir.directory(new File(System.getProperty("user.dir") + "/toit_firmware/"));
+
+        log.info("Creating firmware folder for device: {}", uuid);
+        try {
+            Process process = processBuilderNewDir.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                log.info("Create dir for device: {}", uuid);
+            } else {
+                log.error("Could not create dir for device: {} - with exit code: {}", uuid, exitCode);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+            log.error("Creation interrupted for device: {} - with error: {}", uuid, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void downloadFile(String urlStr, UUID uuid) throws IOException {
+        log.info("Downloading firmware file for device: {}", uuid);
+
         URL url = new URL(urlStr);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         try (InputStream in = connection.getInputStream();
-             FileOutputStream out = new FileOutputStream("toit_firmware/" + firmwareVersion + "/firmware.envelope.gz")) {
+             FileOutputStream out = new FileOutputStream("toit_firmware/" + uuid + "/firmware.envelope.gz")) {
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = in.read(buffer)) != -1) {
@@ -99,10 +126,12 @@ public class FirmwareService {
         }
     }
 
-    private static void gunzipFile(String firmwareVersion) throws IOException {
-        try (FileInputStream fis = new FileInputStream("toit_firmware/" + firmwareVersion + "/firmware.envelope.gz");
+    private static void gunzipFile(UUID uuid) throws IOException {
+        log.info("Unzipping firmware file for device: {}", uuid);
+
+        try (FileInputStream fis = new FileInputStream("toit_firmware/" + uuid + "/firmware.envelope.gz");
              GZIPInputStream gis = new GZIPInputStream(fis);
-             FileOutputStream fos = new FileOutputStream("toit_firmware/" + firmwareVersion + "/firmware.envelope")) {
+             FileOutputStream fos = new FileOutputStream("toit_firmware/" + uuid + "/firmware.envelope")) {
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = gis.read(buffer)) != -1) {
@@ -113,32 +142,11 @@ public class FirmwareService {
         }
     }
 
-    private static void generateFirmwareBin(String firmwareVersion) throws IOException {
-        // Create file for the firmware
-        ProcessBuilder processBuilderNewDir = new ProcessBuilder();
-        processBuilderNewDir.command("mkdir "+ firmwareVersion);
-        processBuilderNewDir.directory(new File(System.getProperty("user.dir") + "/toit_firmware"));
-
-        try {
-            Process process = processBuilderNewDir.start();
-            int exitCode = process.waitFor();
-            if (exitCode == 0) {
-                System.out.println("Create dir: " + firmwareVersion);
-            } else {
-                System.out.println("Could not create dir - exit code: " + exitCode);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-            System.err.println("Creation interrupted: " + e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    private static void generateFirmwareBin(UUID uuid) throws IOException {
         // Run make file to compile
-        System.out.println("Trying to compile...");
+        log.info("Trying to compile... for device: {}", uuid);
         ProcessBuilder processBuilder = new ProcessBuilder();
-        String firmwarePath = "/usr/src/service/toit_firmware/" + firmwareVersion;
+        String firmwarePath = "/usr/src/service/toit_firmware/" + uuid.toString();
         processBuilder.command("make NEW_FIRMWARE_LOCATION=" + firmwarePath);
         processBuilder.directory(new File(System.getProperty("user.dir")));
 
@@ -146,14 +154,14 @@ public class FirmwareService {
             Process process = processBuilder.start();
             int exitCode = process.waitFor();
             if (exitCode == 0) {
-                System.out.println("Compilation successful");
+                log.info("Compilation successful for device: {}", uuid);
             } else {
-                System.out.println("Compilation failed with exit code: " + exitCode);
+                log.error("Compilation successful for device: {} - with exit code: {}", uuid, exitCode);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
             Thread.currentThread().interrupt();
-            System.err.println("Compilation interrupted: " + e.getMessage());
+            log.error("Compilation interrupted for device: {} - with error: {}", uuid, e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
