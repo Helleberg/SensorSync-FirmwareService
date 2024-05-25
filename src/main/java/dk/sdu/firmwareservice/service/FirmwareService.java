ab9b5776374b0698582fc30580832c50e51ab7ab
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-05-26 00:13:23 +0200

    Removed updateFrimwareServiceToitVersion exectution

--- src/main/java/dk/sdu/firmwareservice/service/FirmwareService.java
+++ src/main/java/dk/sdu/firmwareservice/service/FirmwareService.java
@@ -67,7 +67,7 @@ public class FirmwareService {
         try {
             // Check if the Firmware services Toit version is the latest
             // Download new one if not
-            updateFrimwareServiceToitVersion(firmwareVersion);
+            // updateFrimwareServiceToitVersion(firmwareVersion);
 
             // Concatenating the deviceUUID onto the filename to keep track of which device should download it.
             String envelopeUrl = "https://github.com/toitlang/toit/releases/download/" + firmwareVersion + "/firmware-esp32.gz";
